4a724f21f41f77b78715f3321217040b1302d537
nstrong <nstrong@spanimation.com>
2021-06-15 15:34:17 -0700

    express server

--- express/index.js
+++ express/index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import bp from 'body-parser';
 import morgan from 'morgan';
 
 const app = express();
 
-app.use(bp.urlencoded({extended: true}));
-app.use(bp.json());
+app.use(express.json());
 app.use(morgan('dev'));
 
 const db = [
